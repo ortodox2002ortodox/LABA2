ad16d8b841785baaca9e73bd15c7abbc3b6b67ac
ortodox2002ortodox <58324913+ortodox2002ortodox@users.noreply.github.com>
2021-02-10 13:15:35 +0200

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Занятие__2
+{
+    class Person
+    {
+        /* поля/члены без модификтора по умолчанию private  
+               string name; -> имеет параметр private 
+
+        */
+        public string name;
+        private string _name; // для доступа создают специальные методы. смотреть ниже
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void SetNAme(string _name)
+        {
+            _name = name;
+        }
+    }
+}
